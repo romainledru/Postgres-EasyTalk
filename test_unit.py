8a4e7af6ee09893018a8f5cd15533ff2844598dc
Romain <romain.ledru2@gmail.com>
2021-01-04 21:51:05 +0100

    test unit works

--- test_unit.py
+++ test_unit.py
@@ -8,15 +8,22 @@ from table import Table
 class test_Table:
 
     def test_tableCreate(self):
+        pattern1 = {
+            'length': 300,
+        }
+        pattern2 = {
+            'compulsory': False,
+            'length': 50,
+        }
         db = Table("table1")
-        db.add_varcharField("varChar1", 300)
-        db.add_varcharField("varChar2", 50)
+        db.add_varcharField("varChar1", pattern1)
+        db.add_varcharField("varChar2", pattern2)
         db.add_booleanField("bool1")
         db.add_intField("int1")
         db.add_floatField("float1")
-        output = db.create_TABLE()
+        output = db.write_TABLE()
 
-        answer = 'CREATE TABLE table1 (varChar1 varchar(255),varChar2 varchar(50),bool1 boolean,int1 int,float1 float)'
+        answer = 'CREATE TABLE table1 (varChar1 varChar(255), varChar2 varChar(50), bool1 bool, int1 int, float1 float, id int)'
         assert output == answer
     
 ### INSERT ###
@@ -27,12 +34,12 @@ class test_Insertt:
 
     def test_Insert(self):
         db = Table("table1")
-        db.add_varcharField("varChar1", 300)
-        db.add_varcharField("varChar2", 50)
+        db.add_varcharField("varChar1")
+        db.add_varcharField("varChar2")
         db.add_booleanField("bool1")
         db.add_intField("int1")
         db.add_floatField("float1")
-        table = db.create_TABLE()
+        output = db.write_TABLE()
 
         insert = Insert(db)
         entry = {
@@ -44,5 +51,5 @@ class test_Insertt:
         }
         output = insert.write_ENTRY(entry)
 
-        answer = 'INSERT INTO table1 (varChar1,varChar2,bool1,int1,float1) VALUES (hello1,hello2,True,35,40.14)'
+        answer = 'INSERT INTO table1 (varChar1, varChar2, bool1, int1, float1, id) VALUES (hello1, hello2, True, 35, 40.14, None)'
         assert output == answer
