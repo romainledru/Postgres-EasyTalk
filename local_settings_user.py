6542867aa2ca412db39894f4b42e4fb977a7a3cd
Romain <romain.ledru2@gmail.com>
2021-01-10 13:57:59 +0100

    db definition deleted (not used anymore)

--- local_settings_user.py
+++ local_settings_user.py
@@ -1,5 +1,3 @@
-import datetime
-
 ## SETTINGS TO BE CHANGED FOR USER
 
 are_you_admin = True # CHANGEME
@@ -12,32 +10,4 @@ else:
         'port': 'CHANGEME',
         'user': 'CHANGEME',
         'password': 'CHANGEME',
-        'database': 'CHANGEME'
     }
-
-
-## LOCAL POSTGRESQL DEFINITION
-
-pg_items = {
-    'serial': 'serial',
-    'text': str,
-    'integer': int,
-    'real': float,
-    'boolean': bool,
-    'timestamp with time zone': datetime.datetime,
-}
-
-pg_items_str = {
-    'serial': 'serial',
-    'text': 'str',
-    'integer': 'int',
-    'real': 'float',
-    'boolean': 'bool',
-    'timestamp with time zone': 'datetime',
-}
-
-## LOCAL MYSQL DEFINITION
-
-## LOCAL SQLITE DEFINITION
-
-## ...
