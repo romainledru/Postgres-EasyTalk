9dd65fd34a9038490ec4bc17739795ba5c7d2414
Romain <romain.ledru2@gmail.com>
2021-01-13 10:17:48 +0100

    add: test datetime

--- test_unit.py
+++ test_unit.py
@@ -82,4 +82,50 @@ class Test_Table:
         else:
             raise UnitError(problemTag, 'error in pattern')
 
+## TEST DATETIME ##
+    def test_create_addDatetime(self):
 
+        nameId = 'created_at'
+
+        table = Table('easyTalk-db','tabletest')
+        table.add_datetimeField(nameId)
+        table.write_TABLE()
+        tablePick = Table('easyTalk-db','tabletest')
+        try:
+            if nameId not in tablePick.patron:
+                raise
+        except:
+            raise UnitError(nameId,'not found in patron')
+        finally:
+            tablePick.drop_TABLE()
+        
+        assert True # if no problems occurs until here
+
+    def test_create_datetime_pattern(self):
+
+        nameId = 'created_at'
+        table = Table('easyTalk-db','tabletest')
+        table.add_datetimeField(nameId)
+        table.write_TABLE()
+
+        tablePick = Table('easyTalk-db','tabletest')
+
+        pattern = {
+            'compulsory': False,
+            'primary': False,
+            'type': datetime.datetime,
+        }
+        problemTag = ''
+        try:
+            for key, value in tablePick.patron[nameId].items():
+                if tablePick.patron[nameId][key] != pattern[key]:
+                    problemTag = '{},{}'.format(key, value)
+        except:
+            raise UnitError(problemTag, 'error in try')
+        finally:
+            tablePick.drop_TABLE()
+        
+        if problemTag == '':
+            assert True
+        else:
+            raise UnitError(problemTag, 'error in pattern')
