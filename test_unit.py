1a011d93b6f17bd1c1d83d3d01273dc53fd88049
Romain <romain.ledru2@gmail.com>
2021-01-13 10:17:10 +0100

    remove: useless tests

--- test_unit.py
+++ test_unit.py
@@ -33,6 +33,8 @@ class Test_Table:
         finally:
             tablePick.drop_TABLE()
 
+
+## TEST SERIAL ##
     def test_create_addSerialId(self):
 
         nameId = 'id'
@@ -71,7 +73,7 @@ class Test_Table:
                 if tablePick.patron[nameId][key] != pattern[key]:
                     problemTag = '{},{}'.format(key, value)
         except:
-            raise UnitError(problemTag, 'error in pattern')
+            raise UnitError(problemTag, 'error in try')
         finally:
             tablePick.drop_TABLE()
         
@@ -81,82 +83,3 @@ class Test_Table:
             raise UnitError(problemTag, 'error in pattern')
 
 
-
-
-"""
-### TABLE ###
-
-from easytalk.table import Table
-
-class test_Table:
-
-    def test_tableCreate(self):
-        pattern1 = {
-            'length': 300,
-        }
-        pattern2 = {
-            'compulsory': False,
-            'length': 50,
-        }
-        db = Table("table1")
-        db.add_varcharField("varChar1", pattern1)
-        db.add_varcharField("varChar2", pattern2)
-        db.add_booleanField("bool1")
-        db.add_intField("int1")
-        db.add_floatField("float1")
-        output = db.write_TABLE()
-
-        answer = 'CREATE TABLE table1 (varChar1 VARCHAR(255), varChar2 VARCHAR(50), bool1 BOOLEAN, int1 INT, float1 REAL, id INT);'
-        assert output == answer
-    
-### INSERT ###
-
-from easytalk.insert import Insert
-
-class test_Insert:
-
-    def test_Insert(self):
-        db = Table("table1")
-        db.add_varcharField("varChar1")
-        db.add_varcharField("varChar2")
-        db.add_booleanField("bool1")
-        db.add_intField("int1")
-        db.add_floatField("float1")
-        output = db.write_TABLE()
-
-        insert = Insert(db)
-        entry = {
-            'varChar1': 'hello1',
-            'varChar2': 'hello2',
-            'bool1': True,
-            'int1': 35,
-            'float1': 40.14,
-        }
-        output = insert.write_ENTRY(entry)
-
-        answer = 'INSERT INTO table1 (varChar1, varChar2, bool1, int1, float1, id) VALUES (hello1, hello2, True, 35, 40.14, None);'
-        assert output == answer
-
-### SELECT ###
-
-from easytalk.select import Read
-
-class test_Select:
-
-    def test_Select(self):
-        t = Table('tableTest')
-        t.add_varcharField('name')
-        t.add_intField('price')
-        t.add_booleanField('buyORnot')
-
-        s = Read(t)
-        p = {
-            'name': 'jouet',
-            'price': 35,
-        }
-        l = ['name', 'buyORnot']
-        output = s.find_filter(p, l)
-
-        answer = "SELECT name, buyORnot FROM tableTest WHERE (name='jouet') AND (price=35);"
-        assert output == answer
-"""
