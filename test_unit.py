eb5591b1d8d44fc1436d8abec3fc1009a7ffe339
Romain <romain.ledru2@gmail.com>
2021-01-13 10:18:25 +0100

    add: test varchar

--- test_unit.py
+++ test_unit.py
@@ -129,3 +129,51 @@ class Test_Table:
             assert True
         else:
             raise UnitError(problemTag, 'error in pattern')
+
+## TEST VARCHAR ##
+    def test_create_addVarchar(self):
+
+        nameId = 'varchar1'
+
+        table = Table('easyTalk-db','tabletest')
+        table.add_varcharField(nameId)
+        table.write_TABLE()
+        tablePick = Table('easyTalk-db','tabletest')
+        try:
+            if nameId not in tablePick.patron:
+                raise
+        except:
+            raise UnitError(nameId,'not found in patron')
+        finally:
+            tablePick.drop_TABLE()
+        
+        assert True # if no problems occurs until here
+
+    def test_create_varchar_pattern(self):
+
+        nameId = 'varchar1'
+        table = Table('easyTalk-db','tabletest')
+        table.add_varcharField(nameId)
+        table.write_TABLE()
+
+        tablePick = Table('easyTalk-db','tabletest')
+
+        pattern = {
+            'compulsory': True,
+            'primary': False,
+            'type': str,
+        }
+        problemTag = ''
+        try:
+            for key, value in tablePick.patron[nameId].items():
+                if tablePick.patron[nameId][key] != pattern[key]:
+                    problemTag = '{},{}'.format(key, value)
+        except:
+            raise UnitError(problemTag, 'error in try')
+        finally:
+            tablePick.drop_TABLE()
+        
+        if problemTag == '':
+            assert True
+        else:
+            raise UnitError(problemTag, 'error in pattern')
